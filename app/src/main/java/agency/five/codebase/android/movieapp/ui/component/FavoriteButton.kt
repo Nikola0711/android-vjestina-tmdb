4f54f6b15e814d93e0673860517b19697080259a
Nikola0711 <57724179+Nikola0711@users.noreply.github.com>
2022-11-16 19:07:44 +0100

    Zadaca1-ispravak v3

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -2,13 +2,16 @@ package agency.five.codebase.android.movieapp.ui.component
 
 
 import agency.five.codebase.android.movieapp.R
+import agency.five.codebase.android.movieapp.ui.theme.Blue
+import agency.five.codebase.android.movieapp.ui.theme.spacing
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,19 +20,24 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun FavoriteButton(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onClick: () -> Unit
+
 
 ) {
-    val isFavorite = remember {
-        mutableStateOf(false)
-    }
+
+
     Image(
-        painter = painterResource(id = if (isFavorite.value) R.drawable.ic_liked else R.drawable.ic_notliked),
+        painter = painterResource(id = if (isFavorite) R.drawable.ic_liked else R.drawable.ic_notliked),
         contentDescription = "Liked",
         modifier = modifier
-            .padding(15.dp)
-            .size(25.dp)
-            .clickable { isFavorite.value = isFavorite.value.not() }
+            .clickable { onClick() }
+            .background(
+                color = Blue.copy(alpha = 0.6f),
+                shape = CircleShape
+            )
+            .padding(MaterialTheme.spacing.small)
 
 
     )
@@ -38,7 +46,11 @@ fun FavoriteButton(
 @Preview(showBackground = true)
 @Composable
 fun FavoriteButtonPreview() {
-    FavoriteButton()
+    val favButtonModifier = Modifier
+        .padding(10.dp)
+        .size(28.dp)
+
+    FavoriteButton(onClick = {}, isFavorite = false, modifier = favButtonModifier)
 }
 
 
